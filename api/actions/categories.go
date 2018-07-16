2e3870ae3307d08458891a79ac2ad2a52b1d1f9a
Kostiantyn Pinchuk <apollon@users.noreply.github.com>
2018-07-16 23:26:20 +0300

    added requesting category list

--- api/actions/categories.go
+++ api/actions/categories.go
@@ -1,13 +1,22 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"github.com/apollon/workshops/api/models"
+	"github.com/gobuffalo/buffalo"
+	"github.com/pkg/errors"
+)
 
 // CategoriesList default implementation.
 func CategoriesList(c buffalo.Context) error {
-	return c.Render(200, r.HTML("categories/list.html"))
+	categories := []models.Category{}
+	err := models.DB.All(&categories)
+	if err != nil {
+		c.Logger().Error("DB error", errors.WithStack(err))
+	}
+	return c.Render(200, r.JSON(categories))
 }
 
 // CategoriesIndex default implementation.
 func CategoriesIndex(c buffalo.Context) error {
-	return c.Render(200, r.HTML("categories/index.html"))
+	return c.Render(200, r.JSON(map[string]string{"message": "Categories index"}))
 }
