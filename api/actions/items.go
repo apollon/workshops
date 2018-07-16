2233406c9841d19d151e45348eec7630662f938f
Kostiantyn Pinchuk <apollon@users.noreply.github.com>
2018-07-16 23:26:37 +0300

    added requesting items list

--- api/actions/items.go
+++ api/actions/items.go
@@ -1,13 +1,22 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"github.com/apollon/workshops/api/models"
+	"github.com/gobuffalo/buffalo"
+	"github.com/pkg/errors"
+)
 
 // ItemsList default implementation.
 func ItemsList(c buffalo.Context) error {
-	return c.Render(200, r.HTML("items/list.html"))
+	items := []models.Item{}
+	err := models.DB.All(&items)
+	if err != nil {
+		c.Logger().Error("DB error", errors.WithStack(err))
+	}
+	return c.Render(200, r.JSON(items))
 }
 
 // ItemsIndex default implementation.
 func ItemsIndex(c buffalo.Context) error {
-	return c.Render(200, r.HTML("items/index.html"))
+	return c.Render(200, r.JSON(map[string]string{"message": "Items index"}))
 }
